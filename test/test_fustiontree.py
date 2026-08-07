3f081f69a99634917416fde64d8f8011a0d7fdc0
jkr11 <113939131+jkr11@users.noreply.github.com>
2026-08-07 13:07:06 +0200

    refactor tests

--- test/test_fustiontree.py
+++ test/test_fustiontree.py
@@ -1,15 +1,27 @@
 import pytest
-from symmnet.fusiontree import FusionTree, NodeType, TreeSort
+from symmnet.fusiontree import FusionTree, NodeType, TreeSort, OpenEdge, InternalEdge
 from symmnet.symmetry import SU2
 import symmnet.symmetry as symmetry
+
+
+# --- Helpers to generate dataclasses quickly for testing ---
+def make_open(edges: list) -> list[OpenEdge]:
+  return [OpenEdge(name=e, number=i + 1, direction=-1) for i, e in enumerate(edges)]
+
+
+def make_internal(edges: list) -> list[InternalEdge]:
+  return [InternalEdge(name=e, number=i + 1) for i, e in enumerate(edges)]
+
+
+# -----------------------------------------------------------
 
 
 @pytest.fixture
 def fresh_tree_5():
   sym = SU2()
   return FusionTree(
-    open_edges=[-1, -2, -3, -4, -5],
-    internal_edges=["i1", "i2"],
+    open_edges=make_open([-1, -2, -3, -4, -5]),
+    internal_edges=make_internal(["i1", "i2"]),
     nodes=[(-1, -2, "i1"), ("i1", -3, "i2"), ("i2", -4, -5)],
     directions=[NodeType.fusion, NodeType.fusion, NodeType.fusion],
     symmetry=sym,
@@ -19,14 +31,20 @@ def fresh_tree_5():
 @pytest.fixture
 def left_aligned_tree():
   return FusionTree(
-    open_edges=[-1, -2, -3, -4], internal_edges=["i"], nodes=[(-1, -2, "i"), ("i", -3, -4)], directions=[NodeType.fusion, NodeType.fusion]
+    open_edges=make_open([-1, -2, -3, -4]),
+    internal_edges=make_internal(["i"]),
+    nodes=[(-1, -2, "i"), ("i", -3, -4)],
+    directions=[NodeType.fusion, NodeType.fusion],
   )
 
 
 @pytest.fixture
 def right_aligned_tree():
   return FusionTree(
-    open_edges=[-1, -2, -3, -4], internal_edges=["i"], nodes=[(-1, "i", -4), (-2, -3, "i")], directions=[NodeType.fusion, NodeType.fusion]
+    open_edges=make_open([-1, -2, -3, -4]),
+    internal_edges=make_internal(["i"]),
+    nodes=[(-1, "i", -4), (-2, -3, "i")],
+    directions=[NodeType.fusion, NodeType.fusion],
   )
 
 
@@ -49,10 +67,10 @@ def test_paper_equation_4_11_transformation():
   open_legs = [-1, -2, -3, -4, -5, -6, -7, -8]
 
   tau_i = FusionTree(
-    open_edges=open_legs,
-    internal_edges=[1, 2, 3, 4, 5],
+    open_edges=make_open(open_legs),
+    internal_edges=make_internal([1, 2, 3, 4, 5]),
     nodes=[
-      (-2, -3, 1),
+      (-2, -3, 1) ,
       (1, -4, 2),
       (-1, 2, 3),
       (3, 4, 5),
@@ -64,8 +82,8 @@ def test_paper_equation_4_11_transformation():
   )
 
   tau_f = FusionTree(
-    open_edges=open_legs,
-    internal_edges=[1, 2, 3, 4, 5],
+    open_edges=make_open(open_legs),
+    internal_edges=make_internal([1, 2, 3, 4, 5]),
     nodes=[
       (-2, -3, 1),
       (1, -4, 2),
@@ -79,7 +97,6 @@ def test_paper_equation_4_11_transformation():
   )
 
   tau_i.fmove(4)
-
   tau_i.fmove(5)
   assert tau_i.nodes == tau_f.nodes
 
@@ -88,8 +105,8 @@ def test_yoga_fmove():
   open_edges = [-a for a in range(1, 7)]
 
   figure_50_tree = FusionTree(
-    open_edges=open_edges,
-    internal_edges=[1, 2, 3],
+    open_edges=make_open(open_edges),
+    internal_edges=make_internal([1, 2, 3]),
     nodes=[(-1, -2, 1), (1, -4, 2), (2, -3, 3), (3, -5, -6)],
     directions=[NodeType.fusion, NodeType.splitting, NodeType.fusion, NodeType.splitting],
   )
@@ -101,14 +118,20 @@ def test_yoga_fmove():
 @pytest.fixture
 def minimal_monster_tree():
   return FusionTree(
-    open_edges=[-1, -2, -3, -4], internal_edges=["i"], nodes=[(-1, "i", -2), ("i", -3, -4)], directions=[NodeType.splitting, NodeType.fusion]
+    open_edges=make_open([-1, -2, -3, -4]),
+    internal_edges=make_internal(["i"]),
+    nodes=[(-1, "i", -2), ("i", -3, -4)],
+    directions=[NodeType.splitting, NodeType.fusion],
   )
 
 
 @pytest.fixture
 def minimal_monster_tree_right():
   return FusionTree(
-    open_edges=[-1, -2, -3, -4], internal_edges=["i"], nodes=[(-1, -2, "i"), (-3, "i", -4)], directions=[NodeType.splitting, NodeType.fusion]
+    open_edges=make_open([-1, -2, -3, -4]),
+    internal_edges=make_internal(["i"]),
+    nodes=[(-1, -2, "i"), (-3, "i", -4)],
+    directions=[NodeType.splitting, NodeType.fusion],
   )
 
 
@@ -128,31 +151,25 @@ def test_charge_sectors_fib():
   }
 
   tree = FusionTree(
-    open_edges=list(outer.keys()),
-    internal_edges=["internal_1"],
+    open_edges=make_open(list(outer.keys())),
+    internal_edges=make_internal(["internal_1"]),
     nodes=[(-1, -2, "internal_1"), ("internal_1", -3, "internal_2")],
     directions=[NodeType.fusion, NodeType.fusion],
     symmetry=sym,
   )
 
   valid_states = tree.determine_internal_charge_sectors(outer)
-  
+
   formatted_sectors = []
   for state in valid_states:
     vector = [state["internal_1"], state["internal_2"], state[-1], state[-2], state[-3]]
     formatted_sectors.append(vector)
 
-  truth = [
-    [0,1,1,1,1],
-    [1,0,1,1,1],
-    [1,1,1,1,1]
-  ]
+  truth = [[0, 1, 1, 1, 1], [1, 0, 1, 1, 1], [1, 1, 1, 1, 1]]
 
   formatted_sectors.sort()
 
   assert formatted_sectors == truth
-
-
 
 
 def test_charge_sectors_su2():
@@ -166,8 +183,8 @@ def test_charge_sectors_su2():
   }
 
   paper_tree = FusionTree(
-    open_edges=[-1, -2, -3, -4],
-    internal_edges=["internal_1"],
+    open_edges=make_open([-1, -2, -3, -4]),
+    internal_edges=make_internal(["internal_1"]),
     nodes=[(-1, -2, "internal_1"), ("internal_1", -3, -4)],
     directions=[NodeType.fusion, NodeType.fusion],
     symmetry=sym,
@@ -206,10 +223,10 @@ def test_charge_sectors_su2():
 
 # def test_pentagon():
 #   left_tree = FusionTree(
-#     open_edges=[-1, -2, -3, -4, -5],
-#     internal_edges=["i1", "i2"],
-#     nodes=[(-1, -2, "i1"), ("i1", -3, "i2"), ("i2", -4, -5)],
-#     directions=(NodeType.fusion, NodeType.fusion, NodeType.fusion),
+#     list_of_open_edges=make_open([-1, -2, -3, -4, -5]),
+#     list_of_internal_edges=make_internal(["i1", "i2"]),
+#     fusion_tree=[(-1, -2, "i1"), ("i1", -3, "i2"), ("i2", -4, -5)],
+#     fusion_tree_directions=(NodeType.fusion, NodeType.fusion, NodeType.fusion),
 #   )
 #
 #   left_tree.fmove("i1")
@@ -217,14 +234,14 @@ def test_charge_sectors_su2():
 #   left_tree.fmove("i1")
 #
 #   right_tree = FusionTree(
-#     open_edges=[-1, -2, -3, -4, -5],
-#     internal_edges=["i1", "i2"],
-#     nodes=[(-1, -2, "i1"), ("i1", -3, "i2"), ("i2", -4, -5)],
-#     directions=(NodeType.fusion, NodeType.fusion, NodeType.fusion),
+#     list_of_open_edges=make_open([-1, -2, -3, -4, -5]),
+#     list_of_internal_edges=make_internal(["i1", "i2"]),
+#     fusion_tree=[(-1, -2, "i1"), ("i1", -3, "i2"), ("i2", -4, -5)],
+#     fusion_tree_directions=(NodeType.fusion, NodeType.fusion, NodeType.fusion),
 #   )
 #   right_tree.fmove(edge='i2')
 #   right_tree.fmove('i1')
 #
 #   print("This one will fail because we are not checking for relabeling")
 #   # TODO: implement a function same_up_to_relabeling.
-#   assert left_tree.nodes == right_tree.nodes
+#   assert left_tree.fusionTree == right_tree.fusionTree
